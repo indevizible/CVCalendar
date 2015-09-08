a91ace50f0f60b585dca0b6495fc19dfe88eae57
Nattawut Singhchai <wut@2bsimple.com>
2015-09-08 14:11:53 +0700

    fixed for swift2.0

--- CVCalendar Demo/CVCalendar Demo/ViewController.swift
+++ CVCalendar Demo/CVCalendar Demo/ViewController.swift
@@ -60,7 +60,7 @@ extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
     
     func didSelectDayView(dayView: CVCalendarDayView) {
         let date = dayView.date
-        println("\(calendarView.presentedDate.commonDescription) is selected!")
+        print("\(calendarView.presentedDate.commonDescription) is selected!")
     }
     
     func presentedDateUpdated(date: CVDate) {
@@ -142,7 +142,7 @@ extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
 
 // MARK: - CVCalendarViewDelegate
 
-extension ViewController: CVCalendarViewDelegate {
+extension ViewController {
     func preliminaryView(viewOnDayView dayView: DayView) -> UIView {
         let circleView = CVAuxiliaryView(dayView: dayView, rect: dayView.bounds, shape: CVShape.Circle)
         circleView.fillColor = .colorFromCode(0xCCCCCC)
@@ -166,7 +166,7 @@ extension ViewController: CVCalendarViewDelegate {
         let ringLineWidth: CGFloat = 4.0
         let ringLineColour: UIColor = .blueColor()
         
-        var newView = UIView(frame: dayView.bounds)
+        let newView = UIView(frame: dayView.bounds)
         
         let diameter: CGFloat = (newView.bounds.width) - ringSpacing
         let radius: CGFloat = diameter / 2.0
@@ -180,7 +180,7 @@ extension ViewController: CVCalendarViewDelegate {
         ringLayer.lineWidth = ringLineWidth
         ringLayer.strokeColor = ringLineColour.CGColor
         
-        var ringLineWidthInset: CGFloat = CGFloat(ringLineWidth/2.0) + ringInsetWidth
+        let ringLineWidthInset: CGFloat = CGFloat(ringLineWidth/2.0) + ringInsetWidth
         let ringRect: CGRect = CGRectInset(rect, ringLineWidthInset, ringLineWidthInset)
         let centrePoint: CGPoint = CGPointMake(ringRect.midX, ringRect.midY)
         let startAngle: CGFloat = CGFloat(-π/2.0)
